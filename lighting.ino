8f2c1df0542395188648e2e4d074750813e4133c
lelokong <lelokong@hotmail.com>
2021-06-28 01:49:17 -0500

    Update lighting.ino

--- lighting.ino
+++ lighting.ino
@@ -14,12 +14,14 @@ void setup() {
   // program running at 250 ms delay, so our timer must be multipy by 4 to get 1 second
   lightOnTimer *= 4;
   
-  // PIR on pin 2 through 7
+  // PIR on pin 2 through 6
   pinMode(2, INPUT);
   pinMode(3, INPUT);
   pinMode(4, INPUT);
   pinMode(5, INPUT);
   pinMode(6, INPUT);
+  
+  // Physical Light Switch
   pinMode(7, INPUT_PULLUP);
 
   // Relay pin for light
@@ -28,10 +30,14 @@ void setup() {
 }
 
 void loop() {
+
+  // timing for light on
   timer++;
 
-  // search for high input on pin 2 thourgh 7
+  // use to detect any light on triggers, PIR or light switch
   int trigger = 0;
+
+  // search for high input on pin 2 thourgh 7
   for (int i = 2; i < 5; i++) {
     if (digitalRead(i) == 1) 
       trigger = 1;
@@ -46,12 +52,10 @@ void loop() {
     turnOn = 1;
     timer = 0;    
     digitalWrite(lightPin, 0);
-    digitalWrite(LED_BUILTIN, 1);
   }
-  else if (timer == lightOnTimer && turnOn == 1) {
+  else if (timer > lightOnTimer && turnOn == 1) {
     turnOn = 0;
     digitalWrite(lightPin, 1);
-    digitalWrite(LED_BUILTIN, 0);
   }
 
   delay(250);
